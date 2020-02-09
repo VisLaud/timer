60b19560717c8207aba3b50f86cc3761842d9771
Simanta Thapa <44049186+VisLaud@users.noreply.github.com>
2020-02-08 23:48:05 -0800

    added the border around the timer

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
+const durationInput = document.querySelector("#duration");
+const startButton = document.querySelector("#start");
+const pauseButton = document.querySelector("#pause");
+const circle = document.querySelector("circle");
 
-const durationInput = document.querySelector('#duration');
-const startButton = document.querySelector('#start');
-const pauseButton = document.querySelector('#pause');
+const perimeter = circle.getAttribute("r") * 2 * Math.PI;
+circle.setAttribute("stroke-dasharray", perimeter);
 
+let currentOffset = 0;
 const timer1 = new Timer(durationInput, startButton, pauseButton, {
-    onStart() {
-        console.log('Timer Started')
-    }, 
-    onTick() {
-        console.log('Time changed')
-    }, 
-    onComplete() {
-        console.log('Timer completed')
-    }
+  onStart() {
+    console.log("Timer Started");
+  },
+  onTick() {
+    circle.setAttribute("stroke-dashoffset", currentOffset);
+    currentOffset = currentOffset - 1;
+  },
+  onComplete() {
+    console.log("Timer completed");
+  }
 });
